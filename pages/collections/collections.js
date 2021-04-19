a19a341fbe7a6ccc06b3de5b461635105c467b90
Utena <jqzhang777@foxmail.com>
2021-04-19 23:44:31 +0800

    upload my file

---
+++ pages/collections/collections.js
@@ -0,0 +1,75 @@
+// pages/collections/collections.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    search:null,
+    placeholder:"请输入查询的素材...",
+    type:["图片","视频","文案"],
+    tag:["可爱","励志","恋爱","搞笑","治愈","土味"]
+  },
+
+  /**
+   * 搜索函数
+   */
+  searchHandler(e){
+      console.log(e);
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
